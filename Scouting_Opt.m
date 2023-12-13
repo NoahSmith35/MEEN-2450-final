4f8ed6e94c537f0705c21c88059c73495c68ce4a
H-Andes <150394114+H-Andes@users.noreply.github.com>
2023-12-12 17:49:50 -0700

    Update Scouting_Opt.m

--- Scouting_Opt.m
+++ Scouting_Opt.m
@@ -3,44 +3,57 @@ load("VineData.mat")
 load("EnvironmentalForcing.mat")
 findSwitch = 0;
 cost = 0;
-minCost = 1000000;
-for amt = 1:12
-    for speed =.0004:.0001:.5
-        for t = 1:length(tspan) 
-            if mod(t,24) == 0 && findSwitch ==0
-                [infects,infectsFound] = Scouting(speed,amt,vine,t,2);
-                cost = cost + amt*100;
-                if (t-1)/24 > 10
-                    cost = cost + 1000;
-                end
-                if infectsFound == 1 && findSwitch == 0
-                    tFound = t;
-                    findSwitch = 1;
-                    disp('Infection Found')
+loops = 20;
+minCostAll = 1000000*ones(1,loops);
+optAmtAll = zeros(1,loops);
+optSpdAll = zeros(1,loops);
+optTFoundAll = zeros(1,loops);
+for i = 1:loops
+    disp(i)
+    for amt = 12:-1:1
+        for speed =.005:.005:.5
+            for t = 1:length(tspan) 
+                if mod(t,24) == 0 && findSwitch ==0
+                    [infects,infectsFound] = Scouting(speed,amt,vine,t,2);
+                    cost = cost + amt*100;
+                    if (t-1)/24 > 10
+                        cost = cost + 1000;
+                    end
+                    if infectsFound == 1 && findSwitch == 0
+                        tFound = t;
+                        findSwitch = 1;
+    %                     disp('Infection Found')
+                    end
                 end
             end
+            if cost<minCostAll(i)
+                minCostAll(i) = cost;
+                optAmtAll(i) = amt;
+                optSpdAll(i) = speed;
+                optTFoundAll(i) = tFound;
+            end
+            cost = 0;
+            findSwitch = 0;
         end
-        if cost<minCost
-            minCost = cost
-            optAmt = amt
-            optSpeed = speed
-        end
-        cost = 0;
     end
 end
-disp(tFound/24)
+% disp(optTFound/24)
+meanMinCost = mean(minCostAll)
+meanOptAmt = mean(optAmtAll)
+meanOptSpd = mean(optSpdAll)
+meanTFound = mean(optTFoundAll)
+% figure
+% scatter(optAmtAll,optSpdAll)
 
 
 function [infects,infectsFound] = Scouting(speed,amt,vine,t,opts)
-    NpX =10;
-    NpY = 10;
+    NpX = 50;
+    NpY = 50;
     infects = zeros(NpX,NpY);
     infectsFound = 0;
     DetectSize = (20*speed/10)^2/4*pi/5000;
-    Npnts = floor(speed*3600);
     distMax = speed*3600;
     distUsed = 0;
-    currLoc = [0,0];
 %     if opts == 1
 %         gridSize = floor(sqrt(Npnts));
 %         for i = 1:gridSize:NpX
